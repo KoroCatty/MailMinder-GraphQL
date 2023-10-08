14016a5ec62091b67617efc218445d2061d0c46a
KoroCatty <drumer19861018@gmail.com>
2023-10-08 11:15:59 +1100

    test deploy2

--- backend/server.js
+++ backend/server.js
@@ -112,7 +112,6 @@ if (process.env.NODE_ENV === 'production') {
   // Express will serve up the front-end index.html file
   // if it doesn't recognize the route
   // Express が route を認識できない場合は、front-end の index.html ファイルを提供する
-  const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
