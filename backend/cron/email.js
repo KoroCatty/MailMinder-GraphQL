bfeeaee2ef866f012c60e9827ea1ea1b8e9f9e5e
KoroCatty <drumer19861018@gmail.com>
2023-10-10 15:28:09 +1100

    test email3

--- backend/cron/email.js
+++ backend/cron/email.js
@@ -27,7 +27,7 @@ import cron from 'node-cron';
 // const sendEmail = cron.schedule('0 10 * * *', async () => {
 
 //! Send Email every 10 minutes
-const sendEmail = cron.schedule('*/10 * * * *', async () => {
+const sendEmail = cron.schedule('*/5 * * * *', async () => {
 
 //! send email every 10 seconds
 // const sendEmail = cron.schedule('*/10 * * * * *', async () => {
@@ -36,8 +36,6 @@ const sendEmail = cron.schedule('*/10 * * * *', async () => {
 
 //! Send Email at 8:00 AM, 12:00 PM, and 8:00 PM JST every day (日本時間)
 // const sendEmail = cron.schedule('0 23,3,11 * * *', async () => {
-
-
   try {
     // email transport configuration
     const transporter = nodemailer.createTransport({
