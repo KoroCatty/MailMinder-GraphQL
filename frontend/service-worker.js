f16aac844e47f33e3ea9e54d9db64909ae5751a4
KoroCatty <drumer19861018@gmail.com>
2023-11-07 10:54:22 +1100

    PWA test

---
+++ frontend/service-worker.js
@@ -0,0 +1,71 @@
+// self.addEventListener('install', event => {
+//   event.waitUntil(
+//     caches.open('my-pwa-cache').then(cache => {
+//       return cache.addAll([
+//         '/',
+//         '/index.html',
+//         '/styles.css',
+//         '/script.js',
+//         '/logo.jpg'
+//       ]);
+//     })
+//   );
+// });
+
+// self.addEventListener('fetch', event => {
+//   event.respondWith(
+//     caches.match(event.request).then(response => {
+//       return response || fetch(event.request);
+//     })
+//   );
+// });
+
+
+const CACHE_NAME = 'v1';
+const ASSETS_TO_CACHE = [
+  './',  // root path for the app
+  './assets/index-4c06471d.js',
+  './assets/index-c858ac5c.css',
+  './logo.jpg',
+  './index.html'
+];
+
+self.addEventListener('install', (event) => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache:', CACHE_NAME);
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
