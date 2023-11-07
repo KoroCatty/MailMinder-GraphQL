2aaee866d73a68ad487806e6b6a9e00eec2cf5bf
KoroCatty <drumer19861018@gmail.com>
2023-11-07 15:35:02 +1100

    test pwa

--- frontend/service-worker.js
+++ frontend/service-worker.js
@@ -1,76 +1,53 @@
-// self.addEventListener('install', event => {
+// const CACHE_NAME = 'v1';
+// const ASSETS_TO_CACHE = [
+//   './',  // root path for the app
+//   './assets/*.js',
+//   './assets/*.css',
+//   './logo.jpg',
+//   './index.html',
+// ];
+
+// // Service worker Install (Once downloaded, you can't see it again)
+// self.addEventListener('install', (event) => {
+//   console.log("Service worker has been installed")
+//   // Perform install steps
 //   event.waitUntil(
-//     caches.open('my-pwa-cache').then(cache => {
-//       return cache.addAll([
-//         '/',
-//         '/index.html',
-//         '/styles.css',
-//         '/script.js',
-//         '/logo.jpg'
-//       ]);
-//     })
+//     caches.open(CACHE_NAME)
+//       .then((cache) => {
+//         console.log('Opened cache:', CACHE_NAME);
+//         return cache.addAll(ASSETS_TO_CACHE);
+//       })
 //   );
 // });
 
-// self.addEventListener('fetch', event => {
+// // Activate Service Worker
+// self.addEventListener('fetch', (event) => {
 //   event.respondWith(
-//     caches.match(event.request).then(response => {
-//       return response || fetch(event.request);
-//     })
+//     caches.match(event.request)
+//       .then((response) => {
+//         // Cache hit - return response
+//         if (response) {
+//           return response;
+//         }
+//         return fetch(event.request);
+//       })
 //   );
 // });
 
+// // Activate Event (古いキャッシュを削除)
+// self.addEventListener('activate', (event) => {
+//   const cacheWhitelist = [CACHE_NAME];
 
-const CACHE_NAME = 'v1';
-const ASSETS_TO_CACHE = [
-  './',  // root path for the app
-  // './assets/index-4c06471d.js',
-  // './assets/index-c858ac5c.css',
-  // './logo.jpg',
-  // './index.html'
-];
-
-// Service worker Install (Once downloaded, you can't see it again)
-self.addEventListener('install', (event) => {
-  console.log("Service worker has been installed")
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache:', CACHE_NAME);
-        return cache.addAll(ASSETS_TO_CACHE);
-      })
-  );
-});
-
-// Activate Service Worker
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
-});
-
-// Activate Event (古いキャッシュを削除)
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-}
-);
+//   event.waitUntil(
+//     caches.keys().then((cacheNames) => {
+//       return Promise.all(
+//         cacheNames.map((cacheName) => {
+//           if (cacheWhitelist.indexOf(cacheName) === -1) {
+//             return caches.delete(cacheName);
+//           }
+//         })
+//       );
+//     })
+//   );
+// }
+// );
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react";
 import dotenv from 'dotenv';
 import path from 'path';
 
+import { VitePWA } from 'vite-plugin-pwa';
+
 // 一つ上の階層の.envファイルのパスを指定して、dotenvで読み込む
 const parentEnvConfig = dotenv.config({
   path: path.resolve(__dirname, '../.env')
@@ -10,7 +12,48 @@ const parentEnvConfig = dotenv.config({
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [ 
+    react(), 
+    VitePWA({
+      injectRegister: 'auto',
+      manifest: {
+        // icons: [
+        //   {
+        //     src: "/pwa-192x192.png",
+        //     sizes: "192x192",
+        //     type: "image/png",
+        //   },
+        //   {
+        //     src: "/pwa-512x512.png",
+        //     sizes: "512x512",
+        //     type: "image/png",
+        //     purpose: "any maskable",
+        //   },
+        // ],
+      },
+      workbox: {
+        // workbox options for generateSW
+        runtimeCaching: [{
+          // urlPattern: ({ url }) => {
+          //   return url.pathname.startsWith('/localhost');
+          // },
+          urlPattern: "https://remindapp.onrender.com/.*",
+          handler: "CacheFirst" as const,
+          options: {
+            cacheName: "MailMinder-cache",
+            expiration: {
+              maxEntries: 100,
+              maxAgeSeconds: 60 * 60 * 24 * 7 // 7日間
+            },
+            cacheableResponse: {
+              statuses: [0, 200]
+            },
+
+          }
+        }]
+      }
+      
+  }) ],
   server: {
     port: 3000,
     proxy: {
